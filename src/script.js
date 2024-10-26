14daacfddd91e82b9c073881a913486bbda6b5e8
Rohit <167241336+rohitcreates@users.noreply.github.com>
2024-10-26 07:48:48 +0000

    Update

--- src/script.js
+++ src/script.js
@@ -10,3 +10,19 @@ function changeText() {
 }
 
 setInterval(changeText,8000);
+
+// This lines of code for carousel product items
+const nextBtn = document.querySelector(".next-btn");
+const prevBtn = document.querySelector(".prev-btn");
+
+nextBtn.addEventListener("click", function () {
+  let products = document.querySelectorAll(".product");
+  document.querySelector(".product-content").appendChild(products[0]);
+});
+
+prevBtn.addEventListener("click", function () {
+  let products = document.querySelectorAll(".product");
+  document
+    .querySelector(".product-content")
+    .prepend(products[products.length - 1]);
+});
